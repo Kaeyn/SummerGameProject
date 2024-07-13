d115a6abb57768744bd564894c841a032de925ce
HuynhTonyy <107403128+HuynhTonyy@users.noreply.github.com>
2024-07-14 00:25:41 +0700

    MainScene - EveryThing - Add : Mọi thứ cơ bản

---
+++ SummerProject/Assets/Scripts/Other/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+    [SerializeField] float speed;
+    [SerializeField] float damage;
+    
+    LogicGameHandler logicGameHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = transform.GetComponent<Rigidbody2D>();
+        logicGameHandler = GameObject.Find("GameLogicHandler").GetComponent<LogicGameHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!logicGameHandler.isGameOver){
+            Vector2 move = new Vector2(10,0).normalized;
+            rigidbody2D.velocity = new Vector2(move.x * speed,move.y* speed ); 
+        }else{
+            rigidbody2D.velocity = new Vector2(0,0);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "enemy"){
+            other.gameObject.GetComponent<Rabbit>().takeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
