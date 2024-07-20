2f28c7e7045d02e9a55cb7ff207be456d9fb5c18
thanhdev <doquocthanh@github.com>
2024-07-20 10:48:30 +0700

    Assets - Other - Add: Thêm tăng tốc bắn cho Player

---
+++ SummerProject/Assets/Script/Other/FireRateUp.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPotion : Enemy
+{
+    [SerializeField] bool isTargeted = false;
+    GameObject player;
+    Vector3 direction = Vector3.zero;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!logicGameHandler.isGameOver)
+        {
+            if (!isTargeted)
+            {
+                // Set direction to move straight down
+                direction = Vector3.down;
+                isTargeted = true;
+            }
+            rigidbody2D.velocity = new Vector2(0, direction.y * speed);
+        }
+        else
+        {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the collision is with the player
+        if (collision.gameObject.tag == "Player")
+        {
+            // Increase player's fire rate
+            PlayerShooting playerShooting = player.GetComponent<PlayerShooting>();
+            playerShooting.IncreaseFireRate(0.02f); // Call the method to increase fire rate
+            Destroy(gameObject);
+        }
+    }
+}
